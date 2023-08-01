fa1fe0ffd0e6e57eed81d06f0dd9e89c41075123
wxmontalvvo <montalvox27@gmail.com>
2023-08-01 11:06:10 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -17,12 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at http://localhost:3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`); // eslint-disable-line no-console
   });
 });
